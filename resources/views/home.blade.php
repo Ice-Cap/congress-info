a23dbaab3ed8395cb1711b99e16f1deeed8e9d63
Ice-Cap <66086363+Ice-Cap@users.noreply.github.com>
2024-07-23 17:53:39 -0600

    styling updates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,18 +8,19 @@
     </head>
     <body>
         <header>
-            <h1>Congress Info</h1>
+            <h1><span>Congress</span> <span>Info</span></h1>
             <nav>
                 <a href="/">Home</a>
             </nav>
         </header>
         <main>
+            <h2 class="heading-2">Recent Bills</h2>
             @foreach($bills as $bill)
                 @php
                     $bill = (object)$bill;
                 @endphp
                 <div class="bill">
-                    <h4 class="heading-3">{{ $bill->title }}</h4>
+                    <h4 class="heading-4">{{ $bill->title }}</h4>
                     <p>Number: {{ $bill->number }}</p>
                     <p>Latest action: {{ $bill->latestAction["text"] }} <span>Date: {{ $bill->latestAction["actionDate"] }}</span></p>
                     <a href="/bill/{{$bill->congress}}/{{$bill->type}}/{{$bill->number}}">View Bill</a>
