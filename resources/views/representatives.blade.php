30a8c7dc261c35526372d7ef376a5890f4db26ba
Canyon Christiansen <icecapwebsites@gmail.com>
2025-01-25 14:29:26 -0700

    handle when a representative doesn't have a depiction

--- resources/views/representatives.blade.php
+++ resources/views/representatives.blade.php
@@ -6,24 +6,30 @@
     @include('layouts.header')
     <main>
         <h2 class="heading-2">Representatives</h2>
-        @foreach ($representatives as $representative)
-            <div class="representative flex-between">
-                <div class="image-container">
-                    <a href="/representative/{{ $representative->bioguideId }}">
-                        <img src="{{ $representative->depiction->imageUrl }}" alt="{{ $representative->name }}">
-                    </a>
+        @if (empty($representatives))
+            <p>No representatives found.</p>
+        @else
+            @foreach ($representatives as $representative)
+                <div class="representative flex-between">
+                    <div class="image-container">
+                        @if (isset($representative->depiction))
+                            <a href="/representative/{{ $representative->bioguideId }}">
+                                <img src="{{ $representative->depiction->imageUrl }}" alt="{{ $representative->name }}">
+                            </a>
+                        @endif
+                    </div>
+                    <div class="info-container">
+                        <h4 class="heading-4">{{ $representative->name }}</h4>
+                        <p>Party: {{ $representative->partyName }}</p>
+                        <p>State: {{ $representative->state }}</p>
+                    </div>
+                    <div class="btn-container flex-column">
+                        <a href="/representative/{{ $representative->bioguideId }}" class="btn">View Details</a>
+                        <a href="/representative/{{ $representative->bioguideId }}/bills" class="btn">View Bills</a>
+                    </div>
                 </div>
-                <div class="info-container">
-                    <h4 class="heading-4">{{ $representative->name }}</h4>
-                    <p>Party: {{ $representative->partyName }}</p>
-                    <p>State: {{ $representative->state }}</p>
-                </div>
-                <div class="btn-container flex-column">
-                    <a href="/representative/{{ $representative->bioguideId }}" class="btn">View Details</a>
-                    <a href="/representative/{{ $representative->bioguideId }}/bills" class="btn">View Bills</a>
-                </div>
-            </div>
-        @endforeach
+            @endforeach
+        @endif
     </main>
 </body>
 
